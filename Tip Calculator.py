866bd4c3174e6e71be743bb833932c905b5ae77f
Pritha Majumder <48787769+Prithatiti@users.noreply.github.com>
2024-11-22 20:48:32 +0530

    Tip Calculator.py

--- Tip Calculator.py
+++ Tip Calculator.py
@@ -2,7 +2,7 @@
 
 print("Welcome to The Tip Calculator!\n")
 
-bill = float(input("What was the total bill of the restaurant?\n"))
+bill = float(input("What was the total bill of the restaurant?\nRs. "))
 tip = int(input("How much tip (in percentage) would you like to give?\n"))
 
 total_bill = bill * ((tip / 100) + 1)
@@ -10,4 +10,4 @@ split = int(input("How many people to split the bill?\n"))
 
 each_pay = round(total_bill / split, 2)
 
-print(f"Each person should pay: ${each_pay}")
+print(f"Each person should pay: Rs. {each_pay}")
