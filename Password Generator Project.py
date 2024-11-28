0a0cf154d753e48ea6886fc8bd1c32270f3cdb64
Pritha Majumder <48787769+Prithatiti@users.noreply.github.com>
2024-11-28 19:27:05 +0530

    Add files via upload

---
+++ Password Generator Project.py
@@ -0,0 +1,31 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!\n")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+my_pass = []
+password = ""
+
+# Adding random letters into my password
+for letter in range(nr_letters):
+    my_pass.append(random.choice(letters))
+
+# Adding random symbols into my password
+for symbol in range(nr_symbols):
+    my_pass.append(random.choice(symbols))
+
+# Adding random numbers into my password
+for number in range(nr_numbers):
+    my_pass.append(random.choice(numbers))
+
+random.shuffle(my_pass)
+for char in my_pass:
+    password += char
+
+print(f"Your final Password: {password}")
